dde095c91e8680a1f3d25da79c287a20382f6fbe
Anup Saud <anupsaud10@gmail.com>
2024-07-11 07:41:01 -0400

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Anup Saud on 2024-07-11.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
